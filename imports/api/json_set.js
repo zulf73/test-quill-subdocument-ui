7d6b9bb512775a61bf9c2a99cca9767dbd73d74c
Zulf Ahmed <zulf@pop-os.localdomain>
2020-12-12 23:04:14 -0600

    added users

---
+++ imports/api/json_set.js
@@ -0,0 +1,16 @@
+export function json_set( path, doc, val ){
+    var keys = path.split(".");
+    var n = keys.size();
+    if (n == 1 ){
+        doc[keys[0]] = val;
+    }
+    return json_set( keys.slice(1,n).join("."), doc[keys[0]], val);
+}
+export function json_get( path, doc) {
+    var keys = path.split(".");
+    var n = keys.size();
+    if (n == 1 ){
+        return(doc[keys[0]]);
+    }
+    return json_get( keys.slice(1,n).join("."), doc[keys[0]]); 
+}
